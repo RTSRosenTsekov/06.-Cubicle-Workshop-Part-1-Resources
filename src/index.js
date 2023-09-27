25bf58c365cbc130afda61b4d37dbe486d0a43cf
Rosen.Tsekov <ros1914@yahoo.com>
2023-09-27 15:33:55 +0300

    Setup static files, trim handlebars views

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-// В терминала инсталираме 
+// В терминала инсталираме
 //1. npm init -y ,
 // 2. npm i express ,
 // 3. npm i nodemon -D ,
 // 4. npm i express-handlebars ,
-// 5. Накрая пишем npm start за да стартираме сървъра 
+// 5. Накрая пишем npm start за да стартираме сървъра
 
 const express = require("express");
 const app = express();
 const handlebarse = require("express-handlebars");
 const PORT = 1080;
+const path = require("path");
 
 // Handlebarse configuration
-app.engine("hbs" , handlebarse.engine({extname:"hbs"}));
+app.engine("hbs", handlebarse.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
+// setup static files да зареди css fails и да се дебъгва 
+const staticFiles = express.static(path.resolve(__dirname, "public"));
+app.use(staticFiles);
 
-app.get("/" , (req,res) => {
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.listen(PORT , ()=> console.log(`Server is running on port ${PORT}...`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
