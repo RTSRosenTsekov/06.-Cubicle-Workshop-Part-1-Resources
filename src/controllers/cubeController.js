b7dc5da944582e5b5fa1bc9e869b58767f9eed25
Rosen.Tsekov <ros1914@yahoo.com>
2023-10-01 15:50:08 +0300

    Add search functionality, add details

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -2,20 +2,30 @@ const router = require("express").Router();
 const cubeService = require("../services/cubeService");
 
 router.get("/create", (req, res) => {
-  console.log(cubeService.getAll());
   res.render("create");
-  // res.send("Hello from cubes");
 });
 
 router.post("/create", (req, res) => {
-  console.log(req.body);
   const { name, description, imageUrl, difficultyLevel } = req.body;
-  cubeService.create({ 
-    name, 
+  cubeService.create({
+    name,
     description,
-     imageUrl, 
-     difficultyLevel: Number(difficultyLevel) });
+    imageUrl,
+    difficultyLevel: Number(difficultyLevel),
+  });
   res.redirect("/");
+});
+
+router.get("/:cubeId/details", (req, res) => {
+  const { cubeId } = req.params;
+  const cube = cubeService.getSingleCube(cubeId);
+
+  if (!cube) {
+    res.redirect("/404");
+    return;
+  }
+
+  res.render("details", { ...cube });
 });
 
 module.exports = router;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,29 +1,46 @@
 const uniqid = require("uniqid");
 const cubes = [
-    {
-        id: 'kbyxdpcln7c7n7e',
-        name: 'pepi',
-        description: 'asd',
-        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp3qrzOft1bpbwNn84yLrxrr6pLLtrwY4NzQ&usqp=CAU',
-        difficultyLevel: 1
-      },
-      {
-        id: 'kbyxdpcln7c86jj',
-        name: 'Gosho',
-        description: 'dsa',
-        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp3qrzOft1bpbwNn84yLrxrr6pLLtrwY4NzQ&usqp=CAU',
-        difficultyLevel: 5
-      },
+  {
+    id: "kbyxdpcln7c7n7e",
+    name: "pepi",
+    description: "asd",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp3qrzOft1bpbwNn84yLrxrr6pLLtrwY4NzQ&usqp=CAU",
+    difficultyLevel: 1,
+  },
+  {
+    id: "kbyxdpcln7c86jj",
+    name: "Gosho",
+    description: "dsa",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp3qrzOft1bpbwNn84yLrxrr6pLLtrwY4NzQ&usqp=CAU",
+    difficultyLevel: 5,
+  },
 ];
 
-exports.getAll = () => {
-  return [...cubes];
+exports.getAll = (search, from, to) => {
+  let filterCubes = [...cubes];
+  if (search) {
+    filterCubes = filterCubes.filter((cube) =>
+      cube.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+  if (from) {
+    filterCubes = filterCubes.filter(
+      (cube) => cube.difficultyLevel >= Number(from)
+    );
+  }
+  if (to) {
+    filterCubes = filterCubes.filter(
+      (cube) => cube.difficultyLevel <= Number(to)
+    );
+  }
+
+  return filterCubes;
 };
 
 exports.create = (cubeData) => {
   const id = uniqid();
-  console.log({ id });
-
   const newCube = {
     id,
     ...cubeData,
@@ -32,4 +49,8 @@ exports.create = (cubeData) => {
   cubes.push(newCube);
 
   return newCube;
+};
+
+exports.getSingleCube = (id) => {
+  return cubes.find((cube) => cube.id === id);
 };
